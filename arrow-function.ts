0ca2a09429453cdc0c7570c7286d12b50d79485b
rasel <russelroy15@gmail.com>
2021-09-10 19:54:31 +0600

    new code added

--- arrow-function.ts
+++ arrow-function.ts
@@ -1,3 +1,7 @@
 function addNew(num1: number, num2: number): number {
   return num1 + num2;
 }
+
+const add2 = (num1: number, num2: number): number => num1 + num2;
+
+const doMath = (x: number, y: number): void => {};
--- js/arrow-function.js
+++ js/arrow-function.js
@@ -2,3 +2,5 @@
 function addNew(num1, num2) {
     return num1 + num2;
 }
+const add2 = (num1, num2) => num1 + num2;
+const doMath = (x, y) => { };
