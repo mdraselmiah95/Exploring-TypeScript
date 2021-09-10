5640e9ddfbae59fc668a69c714e60c9c0f30c051
rasel <russelroy15@gmail.com>
2021-09-10 20:39:44 +0600

    new code added

--- class.ts
+++ class.ts
@@ -5,4 +5,11 @@ class Car {
     this.model = model;
     this.price = price;
   }
+  getTotalPrice(tax: number): number {
+    const taxAmount = (this.price * tax) / 100;
+    const total = this.price + taxAmount;
+    return total;
+  }
 }
+const toyota = new Car("toyota", 10000);
+const totalPrice: number = toyota.getTotalPrice(25);
