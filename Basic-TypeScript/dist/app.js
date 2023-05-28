7bfab8aadb4588f087629b8e809e2963d7266837
mdraselmiah95 <russelroy15@gmail.com>
2023-05-28 20:08:43 +0600

    learning TS

--- Basic-TypeScript/dist/app.js
+++ Basic-TypeScript/dist/app.js
@@ -1,3 +1,9 @@
 "use strict";
 var myName = "Rasel miah";
-console.log(myName);
+var age = 99;
+age = "45";
+age = true;
+console.log(myName, { age: age });
+var playerName = "Shakib";
+playerName = "Shakib";
+console.log({ playerName: playerName });
--- Basic-TypeScript/src/app.ts
+++ Basic-TypeScript/src/app.ts
@@ -1,2 +1,9 @@
 let myName: string = "Rasel miah";
-console.log(myName);
+let age: number | string | boolean | [] = 99;
+age = "45";
+age = true;
+console.log(myName, { age });
+
+let playerName: "Shakib" | "Tamim" = "Shakib";
+playerName = "Shakib";
+console.log({ playerName });
