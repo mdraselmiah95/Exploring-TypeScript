f12d20027f1066e79410249b7c2ee9b73fadec75
mdraselmiah95 <russelroy15@gmail.com>
2023-05-29 23:12:22 +0600

    Learning TypeScript

--- Basic-TypeScript/dist/learn.js
+++ Basic-TypeScript/dist/learn.js
@@ -1,5 +1,9 @@
 "use strict";
 // Array and Tuples in Typescript
+var _a;
+// let player: (string | number | boolean)[]
+// let player: [string, string, number, boolean, string];
+// player = ["Lionel", "Ronaldo", 107, true, "Mohamed"];
 var footballPlayers = [
     "Lionel Messi",
     "Cristiano Ronaldo",
@@ -9,4 +13,32 @@ var footballPlayers = [
 ];
 footballPlayers.push("Ronaldo");
 var newPlayers = footballPlayers.map(function (player, index) { return index + " => " + player; });
-console.log({ newPlayers: newPlayers });
+// console.log({ newPlayers });
+// Object and Enums in Typescript
+var player;
+player = {
+    name: "Messi",
+    age: 35,
+    country: "Argentina",
+    married: true,
+    clubs: ["Barcelona"],
+};
+player = {
+    name: "Neymar",
+    country: "Brazil",
+    married: false,
+    clubs: ["Barcelona", "PSG"],
+};
+player.married = false;
+player.age = player.age + 5;
+player.clubs = (_a = player.clubs) === null || _a === void 0 ? void 0 : _a.map(function (club) { return club.toUpperCase(); });
+// console.log({ player }, player.clubs);
+var Friends;
+(function (Friends) {
+    Friends[Friends["Mia"] = 0] = "Mia";
+    Friends[Friends["Ethan"] = 1] = "Ethan";
+    Friends[Friends["Lily"] = 2] = "Lily";
+    Friends[Friends["Jackson"] = 3] = "Jackson";
+    Friends[Friends["Ava"] = 4] = "Ava";
+})(Friends || (Friends = {}));
+console.log({ Friends: Friends });
--- Basic-TypeScript/src/learn.ts
+++ Basic-TypeScript/src/learn.ts
@@ -1,5 +1,9 @@
 // Array and Tuples in Typescript
 
+// let player: (string | number | boolean)[]
+// let player: [string, string, number, boolean, string];
+// player = ["Lionel", "Ronaldo", 107, true, "Mohamed"];
+
 const footballPlayers: string[] = [
   "Lionel Messi",
   "Cristiano Ronaldo",
@@ -14,4 +18,45 @@ const newPlayers = footballPlayers.map(
   (player, index) => index + " => " + player
 );
 
-console.log({ newPlayers });
+// console.log({ newPlayers });
+
+// Object and Enums in Typescript
+let player: {
+  name: string;
+  age?: number;
+  country: string;
+  married: boolean;
+  clubs: string[];
+};
+
+player = {
+  name: "Messi",
+  age: 35,
+  country: "Argentina",
+  married: true,
+  clubs: ["Barcelona"],
+};
+
+player = {
+  name: "Neymar",
+  country: "Brazil",
+  married: false,
+  clubs: ["Barcelona", "PSG"],
+};
+
+player.married = false;
+
+player.age = player.age! + 5;
+
+player.clubs = player.clubs?.map((club) => club.toUpperCase());
+
+// console.log({ player }, player.clubs);
+
+enum Friends {
+  Mia,
+  Ethan,
+  Lily,
+  Jackson,
+  Ava,
+}
+console.log({ Friends });
