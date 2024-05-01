884ec2691c934bb8cd71cfafa119466cd9f67249
mdraselmiah95 <russelroy15@gmail.com>
2024-05-01 16:40:06 +0600

    WIP on Learning typescript

---
+++ Basic-TypeScript/dist/Object.js
@@ -0,0 +1,7 @@
+"use strict";
+var commonUser = {
+    company: "Programming Hero",
+    firstName: "Rasel",
+    lastName: "Mia",
+    isMarried: false,
+};
---
+++ Basic-TypeScript/src/Object.ts
@@ -0,0 +1,12 @@
+const commonUser: {
+  company: "Programming Hero"; // type => literal type
+  firstName: string;
+  middleName?: string; // Optional type
+  lastName: string;
+  isMarried?: boolean;
+} = {
+  company: "Programming Hero",
+  firstName: "Rasel",
+  lastName: "Mia",
+  isMarried: false,
+};
