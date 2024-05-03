bfa3b6fae628a45342b4d9b621e87df7e332f437
mdraselmiah95 <russelroy15@gmail.com>
2024-05-04 00:20:55 +0600

    WIP on learning Typescript

--- Basic-TypeScript/dist/Object.js
+++ Basic-TypeScript/dist/Object.js
@@ -5,3 +5,30 @@ var commonUser = {
     lastName: "Mia",
     isMarried: false,
 };
+var addArrow = function (num1, num2) { return num1 + num2; };
+var testUser = {
+    name: "Rafi",
+    balance: 10,
+    addBalance: function (balance) {
+        return "My new balance is :".concat(this.balance + balance);
+    },
+};
+var arrayOne = [1, 2, 3, 4, 5, 6, 7];
+var newArray = arrayOne.map(function (item) { return item * item; });
+// rest operator
+var coolFriends = function () {
+    var friends = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        friends[_i] = arguments[_i];
+    }
+    friends.forEach(function (friend) { return console.log("Hi cool bro ".concat(friend)); });
+};
+coolFriends("Ramim", "Rafi", "Tamim", "Saif", "Shamim", "Saiful", "Arman");
+var stdentOne = {
+    name: "Rasel",
+    age: 28,
+    contact: "01845515895",
+    gender: "male",
+    isMarried: false,
+};
+var addTwo = function (num1, num2) { return num1 + num2; };
--- Basic-TypeScript/src/Object.ts
+++ Basic-TypeScript/src/Object.ts
@@ -10,3 +10,45 @@ const commonUser: {
   lastName: "Mia",
   isMarried: false,
 };
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+const testUser = {
+  name: "Rafi",
+  balance: 10,
+  addBalance(balance: number): string {
+    return `My new balance is :${this.balance + balance}`;
+  },
+};
+
+const arrayOne: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+const newArray: number[] = arrayOne.map((item: number) => item * item);
+
+// rest operator
+
+const coolFriends = (...friends: string[]) => {
+  friends.forEach((friend: string) => console.log(`Hi cool bro ${friend}`));
+};
+
+coolFriends("Ramim", "Rafi", "Tamim", "Saif", "Shamim", "Saiful", "Arman");
+
+type Student = {
+  name: string;
+  age: number;
+  contact: string;
+  gender: string;
+  isMarried: boolean;
+};
+
+const stdentOne: Student = {
+  name: "Rasel",
+  age: 28,
+  contact: "01845515895",
+  gender: "male",
+  isMarried: false,
+};
+
+type Add = (num1: number, num2: number) => number;
+
+const addTwo: Add = (num1, num2) => num1 + num2;
