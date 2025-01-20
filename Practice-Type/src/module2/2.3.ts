a2fb693d69138752de359ee71815c11130ed7aa0
mdraselmiah95 <russelroy15@gmail.com>
2025-01-20 23:17:03 +0600

    FEAT: learing type assertion

---
+++ Practice-Type/src/module2/2.3.ts
@@ -0,0 +1,12 @@
+{
+  //
+
+  //TODO: generic type
+  type GenericArray<param> = Array<param>;
+
+  const rollNumber: GenericArray<number> = [1, 2, 3, 4, 5, 6];
+  const mentors: GenericArray<string> = ["MR Rom", " MR sk", "MR UY", "Mr YY"];
+  const boolArray: GenericArray<boolean> = [true, false, false, true];
+
+  //
+}
