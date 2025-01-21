d7c609158f4bef71c867d8252894c380f1dec3d0
mdraselmiah95 <russelroy15@gmail.com>
2025-01-21 23:07:57 +0600

    FEAT: learning Typescript

--- Practice-Type/src/module2/2.4.ts
+++ Practice-Type/src/module2/2.4.ts
@@ -31,5 +31,25 @@
     },
   };
 
+  const NewDeveloper: Developer<{
+    brand: string;
+    model: string;
+    heartTrack: boolean;
+    sleepTrack: boolean;
+  }> = {
+    name: "Mr Cool Dev",
+    computer: {
+      brand: "HP",
+      model: "Pro book",
+      releaseYear: 2020,
+    },
+    smartWatch: {
+      brand: "Apple watch",
+      model: "watch 2",
+      heartTrack: true,
+      sleepTrack: true,
+    },
+  };
+
   //
 }
