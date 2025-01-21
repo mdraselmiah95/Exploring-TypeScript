9fcbed78e8d14b643567eab243651ef3ee5b5fd8
mdraselmiah95 <russelroy15@gmail.com>
2025-01-21 23:39:03 +0600

    FEAT: learning Typescript

--- Practice-Type/src/module2/2.5.ts
+++ Practice-Type/src/module2/2.5.ts
@@ -6,6 +6,20 @@
     return [param];
   };
 
+  const createArrayWithGeneric = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  type User = {
+    id: number;
+    name: string;
+  };
+
+  const newGenObj = createArrayWithGeneric<User>({
+    id: 234,
+    name: "Rasel",
+  });
+
   const reas1 = createArray("BD");
   //
 }
