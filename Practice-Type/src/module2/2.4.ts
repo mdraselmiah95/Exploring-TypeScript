368da7618e1ecc4d6e8db25c3d49427bf7781224
mdraselmiah95 <russelroy15@gmail.com>
2025-01-21 23:03:21 +0600

    FEAT: learning Typescript

--- Practice-Type/src/module2/2.4.ts
+++ Practice-Type/src/module2/2.4.ts
@@ -13,5 +13,23 @@
     smartWatch: T;
   }
 
+  const coolDeveloper: Developer<{
+    brand: string;
+    model: string;
+    display: string;
+  }> = {
+    name: "Rasel",
+    computer: {
+      brand: "Mac",
+      model: "M2",
+      releaseYear: 2023,
+    },
+    smartWatch: {
+      brand: "Honor",
+      model: "Brand 7",
+      display: "Amoled",
+    },
+  };
+
   //
 }
