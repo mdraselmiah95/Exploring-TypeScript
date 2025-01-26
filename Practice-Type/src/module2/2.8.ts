67c1c5a280754c3b8acf3f8552b2cb362e42839c
mdraselmiah95 <russelroy15@gmail.com>
2025-01-26 23:25:23 +0600

    LEARNING: typescript

--- Practice-Type/src/module2/2.8.ts
+++ Practice-Type/src/module2/2.8.ts
@@ -1,6 +1,15 @@
 {
   //
   // TODO : Promise
+
+  const getTodo = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log({ data });
+  };
+
+  getTodo();
+
   type Something = { something: string };
 
   const createPromise = (): Promise<Something> => {
