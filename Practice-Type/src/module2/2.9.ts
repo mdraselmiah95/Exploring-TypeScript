91516c76bc4b5a1fd33035da3e3f6a4bb73098d3
mdraselmiah95 <russelroy15@gmail.com>
2025-01-29 23:45:56 +0600

    Learing typescript

---
+++ Practice-Type/src/module2/2.9.ts
@@ -0,0 +1,13 @@
+import { util } from "prettier";
+
+{
+  // conditional type
+
+  type a1 = number;
+  type b1 = undefined;
+
+  type x = a1 extends null ? true : false;
+  type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+  //
+}
