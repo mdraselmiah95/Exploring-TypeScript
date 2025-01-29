85e06ef4bc962a190daf81dec12fc78d48280d6f
mdraselmiah95 <russelroy15@gmail.com>
2025-01-29 23:51:10 +0600

    Learing typescript

--- Practice-Type/src/module2/2.9.ts
+++ Practice-Type/src/module2/2.9.ts
@@ -9,5 +9,15 @@ import { util } from "prettier";
   type x = a1 extends null ? true : false;
   type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
 
+  type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+
+  type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+  type HasCar = CheckVehicle<"ship">;
+
   //
 }
