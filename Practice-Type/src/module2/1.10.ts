cde2e3e616ebe36871a2b6cf40de7ad5c1ff587d
mdraselmiah95 <russelroy15@gmail.com>
2025-02-03 22:59:15 +0600

    Learing typescript

--- Practice-Type/src/module2/1.10.ts
+++ Practice-Type/src/module2/1.10.ts
@@ -5,8 +5,21 @@
     width: number;
   };
 
-  type AreaString = {
-    [key in keyof AreaNumber]: boolean;
+  type Height = AreaNumber["height"];
+
+  // keyof AreaNumber=> "height" | "width"
+
+  // T= {height:string,width:number}
+  // key => T["height"] => string
+  // key => T["width"] => number
+
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const area1: AreaString<{ height: string; width: number }> = {
+    height: "10",
+    width: 20,
   };
 
   //
