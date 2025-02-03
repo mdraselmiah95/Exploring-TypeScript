a279cc51cc75130dfe23ecad90f313755355cd3c
mdraselmiah95 <russelroy15@gmail.com>
2025-02-03 23:50:11 +0600

    Learing typescript

---
+++ Practice-Type/src/module3/3.1.ts
@@ -0,0 +1,19 @@
+{
+  // class and object
+
+  // oop - class
+
+  class Animal {
+    name: string;
+    species: string;
+    sound: string;
+
+    constructor(name: string, species: string, sound: string) {
+      this.name = name;
+      this.species = species;
+      this.sound = sound;
+    }
+  }
+
+  //
+}
