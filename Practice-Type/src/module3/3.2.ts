a50034ab986185f996226a78a451864ef97b359a
mdraselmiah95 <russelroy15@gmail.com>
2025-02-04 00:29:03 +0600

    Learing typescript

--- Practice-Type/src/module3/3.2.ts
+++ Practice-Type/src/module3/3.2.ts
@@ -20,5 +20,35 @@
   const student1 = new Student("Mr Rasel", 26, "Dhaka");
 
   student1.getSleep(2);
+
+  class Teacher {
+    name: string;
+    age: number;
+    address: string;
+    designation: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+      this.designation = designation;
+    }
+
+    getSleep(numOfHours: number) {
+      console.log(`${this.name} will sleep for ${numOfHours}`);
+    }
+
+    takeClass(numOfClass: number) {
+      console.log(`${this.name} will take ${numOfClass}`);
+    }
+  }
+
+  const teacher1 = new Teacher("Mr Ibn", 33, "DH", "Math Professor");
+  teacher1.takeClass(4);
   //
 }
