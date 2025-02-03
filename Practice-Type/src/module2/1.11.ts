88f93cb454d3e28c65aa581f984c83f49958a222
mdraselmiah95 <russelroy15@gmail.com>
2025-02-03 23:28:39 +0600

    Learing typescript

--- Practice-Type/src/module2/1.11.ts
+++ Practice-Type/src/module2/1.11.ts
@@ -1,17 +1,31 @@
 {
   // Utility Types
 
-  type Person = {
+  type PersonType = {
     name: string;
     age: number;
-    email: string;
+    email?: string;
     contactNo: string;
   };
 
-  type NameAge = Pick<Person, "name" | "age">;
+  type NameAge = Pick<PersonType, "name" | "age">;
 
   // Omit
-  type ContactInfo = Omit<Person, "name" | "age">;
+  type ContactInfo = Omit<PersonType, "name" | "age">;
+
+  // Required
+  type RequiredPerson = Required<PersonType>;
+
+  // Record
+  type MyObj = {
+    a: string;
+    b: string;
+  };
+
+  const obj1: MyObj = {
+    a: "1",
+    b: "2",
+  };
 
   //
 }
