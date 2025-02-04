158ebf55e81437e69694f0452db517e28da5132e
mdraselmiah95 <russelroy15@gmail.com>
2025-02-04 23:58:41 +0600

    Learing typescript

--- Practice-Type/src/module3/3.3.ts
+++ Practice-Type/src/module3/3.3.ts
@@ -27,8 +27,23 @@
 
   const getUser = (user: NormalUser | AdminUser) => {
     if ("role" in user) {
+      console.log(`My name is ${user.name} and my role is ${user.role}`);
+    } else {
+      console.log(`My name is ${user.name}`);
     }
   };
+
+  const user1: NormalUser = {
+    name: "Mr. Rasel normal",
+  };
+
+  const user2: AdminUser = {
+    name: "Mr. Rasel normal",
+    role: "Admin",
+  };
+
+  getUser(user1);
+  getUser(user2);
 
   //
 }
