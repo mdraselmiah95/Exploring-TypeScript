6df7d101a830557081a9b132d87dbcf1168c9dad
mdraselmiah95 <russelroy15@gmail.com>
2025-02-07 15:18:35 +0600

    Learing typescript

--- Practice-Type/src/module3/3.4.ts
+++ Practice-Type/src/module3/3.4.ts
@@ -34,10 +34,20 @@
     }
   }
 
+  // smart way for handling
+
+  const isDog = (animal: Animal): animal is Dog => {
+    return animal instanceof Dog;
+  };
+
+  const isCat = (animal: Animal): animal is Cat => {
+    return animal instanceof Cat;
+  };
+
   const getAnimal = (animal: Animal) => {
-    if (animal instanceof Dog) {
+    if (isDog(animal)) {
       animal.makeBark();
-    } else if (animal instanceof Cat) {
+    } else if (isCat(animal)) {
       animal.makeMeaw();
     }
   };
