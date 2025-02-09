7ad1431a4a63bca5c39eb22b3286c1633c46b3b0
mdraselmiah95 <russelroy15@gmail.com>
2025-02-09 23:47:58 +0600

    Learing typescript

---
+++ Practice-Type/src/module3/3.10.ts
@@ -0,0 +1,40 @@
+{
+  // Encapsulation in OOP
+
+  class BankAccount {
+    readonly id: number;
+    public name: string;
+    protected _balance: number; // protected and private for encapsulation
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+
+    getHiddenMethod() {
+      return this.getBalance();
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    testAc() {
+      this._balance = this._balance;
+    }
+  }
+
+  const newIbnAcc = new BankAccount(12, "Mr. Ibn", 10000);
+  newIbnAcc.addDeposit(5000);
+
+  const myBala = newIbnAcc.getBalance();
+
+  console.log(myBala);
+}
