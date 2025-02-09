59fa43d26d07dbe726bc95a9249c09b385e9a4c2
mdraselmiah95 <russelroy15@gmail.com>
2025-02-09 23:43:18 +0600

    Learing typescript

--- Practice-Type/src/module3/3.9.ts
+++ Practice-Type/src/module3/3.9.ts
@@ -24,5 +24,29 @@
 
   HondaCar.startEngine();
 
+  abstract class Vehicle2 {
+    abstract startEngine(): void {}
+    abstract stopEngine(): void {}
+    abstract move(): void {}
+
+    test() {
+      console.log(`This is a test.`);
+    }
+  }
+
+  class HondaCar1 extends Vehicle2 {
+    startEngine(): void {
+      console.log(`I am starting the car engine`);
+    }
+    stopEngine(): void {
+      console.log(`I am stoping the car engine`);
+    }
+    move(): void {
+      console.log(`I am moving the car engine`);
+    }
+  }
+
+  // abstract class
+
   //
 }
