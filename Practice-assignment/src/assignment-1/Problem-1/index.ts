58d970af9d11bc7c2e30cb2865d26dc92ebeee48
mdraselmiah95 <russelroy15@gmail.com>
2025-02-10 00:01:13 +0600

    Practicing  typescript

---
+++ Practice-assignment/src/assignment-1/Problem-1/index.ts
@@ -0,0 +1,13 @@
+// TODO: Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+
+function repeatString(str: string, count: number) {
+  return str.repeat(count);
+}
+
+// Sample Input:
+repeatString("Hello!", 3);
+
+console.log(repeatString("Hello!", 3));
+
+// Sample Output:
+("Hello!Hello!Hello!");
