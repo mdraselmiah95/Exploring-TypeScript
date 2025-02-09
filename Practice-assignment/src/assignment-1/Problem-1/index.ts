10cc4369317f2fb8866f4ffba3a4ed2a3b132d6d
mdraselmiah95 <russelroy15@gmail.com>
2025-02-10 00:09:43 +0600

    Practicing  typescript

--- Practice-assignment/src/assignment-1/Problem-1/index.ts
+++ Practice-assignment/src/assignment-1/Problem-1/index.ts
@@ -1,4 +1,4 @@
-// TODO: Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+// TODO: Problem 1: Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
 
 function repeatString(str: string, count: number) {
   return str.repeat(count);
---
+++ Practice-assignment/src/assignment-1/Problem-2/index.ts
@@ -0,0 +1,22 @@
+// TODO :Problem 2: Design a TypeScript function findLargestNumber that takes an array of numbers and returns the largest number in the array.
+
+function findLargestNumber(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Array must not be empty");
+  }
+
+  let largest = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
+}
+// Sample Input:
+findLargestNumber([10, 5, 8, 20, 3]);
+console.log(findLargestNumber([10, 5, 8, 20, 3]));
+
+// Sample Output:
+20;
